8361091e174fa29825fb3472126c3a3013b36b1b
vignesh s <vigneshrevathy27@gmail.com>
2017-11-16 17:51:20 -0800

    All files should be commited on Nov 16

--- blog.js
+++ blog.js
@@ -1,18 +1,42 @@
-function callIndex(){
-    alert("welcome");
-    $.ajax({
-    url: "https://jsonprovider.herokuapp.com/posts",
-    type: "GET",
+//Active link js
+var selector = '#nav li';
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
 
-    contentType: 'application/json',
-    success: function(resultData) {
+//default load of API Get method both index and home page
+    $(document).ready(function(){
+         $.getJSON('https://jsonprovider.herokuapp.com/posts?limit=10', function(data) { //getting multiple objects
+            $.each(data, function(index, obj) { //separating objects by its index
+                var title = obj.title; //object.property
+                    var blog_id = obj.id;
+                    $("#blog_id").append('<img src="images/blog-4.jpg" width="60px" height="60px" />&nbsp;&nbsp;<b>Blog Title:</b>&nbsp;'+title + 
+                    '</br></br> <button onclick="blogfunction('+blog_id+')" id='+blog_id +'>Read More </button> <hr></br>');
+                  });
+              });
+        });
+ 
         
-        $("#blog_id").html(resultData);
-        alert(resultdata);
+//Read more button blog view function
+function blogfunction(id){
+  url = "https://jsonprovider.herokuapp.com/posts/"+id;
+  $.getJSON(url, function(data){
+    var msg = data.body;
+    var title = data.title;
+   $('#main_body').html('<img src="images/blog-4.jpg" width="100px" height="100px" /><br><br><b>Blog Title:</b></b>&nbsp;'
+   +title+'<br><br><b>Blog</b>:&nbsp;'+msg+
+   '<br><br><button onclick="returnfunction()" >Back </button>');
+  });
+}
 
-    },
-    error : function(jqXHR, textStatus, errorThrown) {
-    },
+//home click
+function indexfunction(){
+    $('#main_body').load("home.html");
+}
 
-    timeout: 120000,
-  }
+//Back button 
+function returnfunction(){
+  $('#main_body').load("home.html");
+}
+
